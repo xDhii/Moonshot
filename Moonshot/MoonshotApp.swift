f3f5844450a43b65d68b07d766d7ac14604c6fd1
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-12-22 18:21:27 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Adriano Valumin on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
